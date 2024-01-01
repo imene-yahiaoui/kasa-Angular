0f73da075f9ce0cfa55235c62d942dee940f0c12
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2024-01-01 11:47:27 +0100

    add home test

---
+++ cypress/e2e/home/home.cy.ts
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+it('cy.request() - make an XHR request', () => {
+  cy.visit('http://localhost:4200');
+
+  cy.request('http://localhost:4200').should((response) => {
+    expect(response.status).to.eq(200);
+  });
+});
+
+context('Window', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200');
+  });
+
+  it('cy.window() - get the global window object', () => {
+    cy.window().should('have.property', 'top');
+  });
+
+  it('cy.document() - get the document object', () => {
+    cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
+  });
+
+  it('logo click', () => {
+    cy.get('.logo').click();
+    cy.url().should('include', '/');
+  });
+});
